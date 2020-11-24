bd85068192e5c3cfbe82fdf597deb91b2fae942a
I <62927268+Dev-I-J@users.noreply.github.com>
2020-11-24 18:03:40 +0530

    Update .travis.yml and shovel tasks

--- shovel/app/shovel.py
+++ shovel/app/shovel.py
@@ -1,7 +1,8 @@
 from shovel import task
 
-from shutil import rmtree
+from os import chdir, remove
 from subprocess import run as shell
+from shutil import rmtree, copyfile, copytree
 
 
 @task
@@ -26,14 +27,48 @@ def runApp():
 
 
 @task
+def runAppDebug():
+    shell(".test/dist/JNote/JNote")
+
+
+@task
 def cleanApp():
     rmtree('build', ignore_errors=True)
     rmtree('dist', ignore_errors=True)
 
 
 @task
+def cleanAppDebug():
+    rmtree('.test/build', ignore_errors=True)
+    rmtree('.test/dist', ignore_errors=True)
+    rmtree('.test/icons', ignore_errors=True)
+    remove(".test/fileio.py")
+    remove(".test/settings.py")
+    remove(".test/main.py")
+    remove(".test/main.qml")
+    remove(".test/settings.toml")
+    remove(".test/README.md")
+    remove(".test/LICENSE.md")
+
+
+@task
 def buildApp():
     shell("pipenv run PyInstaller JNote.spec")
+
+
+@task
+def buildAppDebug():
+    copyfile("fileio.py", ".test/fileio.py")
+    copyfile("settings.py", ".test/settings.py")
+    copyfile("main.py", ".test/main.py")
+    copyfile("main.qml", ".test/main.qml")
+    copyfile("settings.toml", ".test/settings.toml")
+    copyfile("README.md", ".test/README.md")
+    copyfile("LICENSE.md", ".test/LICENSE.md")
+    copytree("icons", ".test/icons")
+    chdir(".test")
+    shell("pipenv run PyInstaller JNote.spec")
+    chdir("../")
 
 
 @task
