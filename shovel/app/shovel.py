5b01f30942a9a08d3656765ce827bbdd2debd92e
I <62927268+Dev-I-J@users.noreply.github.com>
2020-11-25 19:48:47 +0530

    Update shovel tasks [mac]

--- shovel/app/shovel.py
+++ shovel/app/shovel.py
@@ -8,7 +8,7 @@ from shutil import rmtree, copyfile, copytree
 @task
 def installDeps(pipPath="pip"):
     """Install Dependencies"""
-    shell([pipPath, "install", "--upgrade", "shovel"])
+    shell([pipPath, "install", "--upgrade", "pipenv"])
     shell(["pipenv", "install", "--dev"])
 
 
